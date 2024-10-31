b49e7bc5e625a27d3e4c192ac3b1da83a157ea76
unknown <atpulkka21@student.oulu.fi>
2024-10-31 16:55:15 +0200

    User Story 1

--- test/test_piglatin.py
+++ test/test_piglatin.py
@@ -7,3 +7,8 @@ class TestPigLatin(unittest.TestCase):
 
     def test_something(self):
         pass
+
+    def test_create_translator_and_get_phrase(self):
+        piglatin = PigLatin("hello world")
+        phrase = piglatin.get_phrase()
+        self.assertEqual("hello world", phrase)
